6621d99b25ea6361aa164ece6ba69cd24ce66d26
Fred George <fredgeorge@acm.org>
2018-10-20 17:01:15 -0700

    Fred: Skeleton Maven project with pom.xml for JUnit 5

---
+++ src/main/java/com/nrkei/app/App.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2017 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+ package com.nrkei.app;
+
+// Hello, World
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/nrkei/app/AppTest.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 by Fred George
+ * May be used freely except for training; license required for training.
+ */
+
+package com.nrkei.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Ensure App works correctly
+class AppTest 
+{
+    @Test void app() {
+        assertTrue( true );
+    }
+}
